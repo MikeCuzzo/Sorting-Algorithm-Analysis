7b4737fe235927338af517741492dc8fecfcf9d1
iso ubunto <mcuzzo@smu.edu>
2021-03-10 12:43:41 -0600

    Starting up

---
+++ cmake-build-debug/CMakeFiles/21s-pa02-MikeCuzzo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "21s-pa02-MikeCuzzo"
+  "21s-pa02-MikeCuzzo.pdb"
+  "CMakeFiles/21s-pa02-MikeCuzzo.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21s-pa02-MikeCuzzo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "AlgorithmStrategy.h"
+
+int main() {
+    AlgorithmStrategy search;
+
+//    for (all Algo Search Types) {
+//        for (all Data Types) {
+//            search.Load(input
+//            file
+//            type);
+//            search.Execute();
+//            search.Stats();
+//        }
+//    }
+
+    return 0;
+}
